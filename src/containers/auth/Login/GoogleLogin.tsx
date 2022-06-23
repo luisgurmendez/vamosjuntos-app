02bde70b2f8ebef86764cd685c107ab4d3d0a658
Luis Gurmendez <luisgurmendez@gmail.com>
2022-06-23 10:15:52 -0300

    .changed package name brought errors in google sign in

--- src/containers/auth/Login/GoogleLogin.tsx
+++ src/containers/auth/Login/GoogleLogin.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import SocialLoginButton from './SocialLoginButton';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 GoogleSignin.configure({
   webClientId: '257892290311-9m7cu8asuhsqigqm5bbvvredcu5ivapt.apps.googleusercontent.com',
@@ -9,9 +10,16 @@ GoogleSignin.configure({
 
 const GoogleLogin: React.FC<{ style?: any }> = ({ style }) => {
   const handleLogin = async () => {
-    const { idToken } = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    await auth().signInWithCredential(googleCredential);
+    try {
+
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      await auth().signInWithCredential(googleCredential);
+    } catch (e) {
+      console.error(e);
+      crashlytics().recordError(e);
+    }
+
   }
 
   return (
